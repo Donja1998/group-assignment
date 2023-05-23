7bebd1c8ec2e100fc70b1f1e5e58f0e182ba4550
stinafreijd <85034588+stinafreijd@users.noreply.github.com>
2023-05-23 16:27:07 +0200

    footer

--- multitier-blog/frontend/Template.php
+++ multitier-blog/frontend/Template.php
@@ -58,6 +58,7 @@ public static function footer()
         <footer>
             <div class="footer-content">
                 <div class="footer-section">
+                <p id="logo">THYNK TRAVEL</p>
                 <img class="footer-logo" src="<?= $home_path ?>/assets/img/logo.png" alt="Logo">
                     <p>Sign up for our newsletter:</p>
                     <input type="email" placeholder="Enter your email" class="newsletter-input">
