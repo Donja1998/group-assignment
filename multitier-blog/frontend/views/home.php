5ec7e4fea41e0719b1e935d391eb7312f8f9f0c1
stinafreijd <85034588+stinafreijd@users.noreply.github.com>
2023-05-25 13:05:12 +0200

    home

--- multitier-blog/frontend/views/home.php
+++ multitier-blog/frontend/views/home.php
@@ -3,10 +3,6 @@ require_once __DIR__ . "/../Template.php";
 
 HomeTemplate::HomeHeader("Home");
 ?>
-<header style="background-image: url('<?= $home_path ?>/assets/img/header-bg.jpg')">
-                <h1><?= $title; ?></h1>
-            </header>
-
 <h1>Welcome home</h1>
 
 
